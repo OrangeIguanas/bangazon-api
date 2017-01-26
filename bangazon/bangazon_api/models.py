dfa415ed93aec16150f474cf5dbe815e874d5b5f
Zachary Cline <zcline08@gmail.com>
2017-01-26 11:39:15 -0600

    Zc--Created Products Class in models.py

--- bangazon/bangazon_api/models.py
+++ bangazon/bangazon_api/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Products(models.Model):
+
+""" Products model class
+    The purpose of this class is to define the Product data model.
+    author: Zach
+    subclasses: Meta (with ordering by name)
+    DecimalField(max_digits=6, decimal_places=2) ex. 9999.99
+    max_digits in decimal field is the total number of digits including the decimal places.
+"""
+    name = models.CharField(max_length=55)
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    description = models.CharField(max_length=140)
+    quantity = models.IntegerField()
+    categoryId = models.ForeignKey(Category)
+    customerId = models.ForeignKey(Customer)
+
+    class Meta:
+        ordering = ('name',)
+
