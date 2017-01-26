33915eb64e9cd40fc188ae266a6d04ef16725dc5
Ikechi <consumerike@gmail.com>
2017-01-26 09:19:02 -0600

    added customers model

--- bangazon/bangazon_api/models.py
+++ bangazon/bangazon_api/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+"""Customers model class
+    The purpose of this class is to define the Customers data model.
+    author: Ike
+    methods: none
+    subclasses: Meta (with ordering by last_name)
+
+"""
+class Customers(models.Model):
+    first_name = CharField(max_length=100, default='')
+    last_name = CharField(max_length =100)
+    created_date = DateTimeField(auto_now_add=True)
+    street_address = TextField()
+    city = CharField(max_length=100)
+    zip_code = IntegerField(max_length=12)
+    state = CharField(max_length=20)
+
+    class Meta:
+        ordering = ('last_name',)
