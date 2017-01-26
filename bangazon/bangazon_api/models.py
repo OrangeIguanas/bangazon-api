73b2bb8eac578be42a9f90e31359979d29cc1ca5
Abby Moore <abbyleighanne@gmail.com>
2017-01-26 11:55:06 -0600

    added PaymentType class

--- bangazon/bangazon_api/models.py
+++ bangazon/bangazon_api/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+
+
+
+class PaymentType(models.Model):
+	"""PaymentType model class
+	    The purpose of this class is to define customer's payment types
+	    author: Abby
+	    methods: none
+	    meta: plural name
+	"""
+	card_type = models.CharField(max_length=30)
+	card_number = models.CharField(max_length=25)
+	cvv = models.CharField(max_length=4)
+	expiration = models.DateField(auto_now=False)
+	billing_name = models.CharField(max_length=55)
+	customer_id = models.ForeignKey(Customer)
+
+	class Meta:
+		verbose_name_plural = "PaymentTypes"
+
