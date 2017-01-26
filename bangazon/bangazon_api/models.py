2b9b25e9ba70f09c5743e64a37e6ea4d6cd99c4f
Shawn M <forgeforza@outlook.com>
2017-01-26 10:38:53 -0600

    created productorder table

--- bangazon/bangazon_api/models.py
+++ bangazon/bangazon_api/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class product_has_order (models.Model):
+	product_id = models.Integerfield
+	order_id = models.Integerfield
+
+class Meta: 
+	ordering = [product_id, order_id]
+
+	
