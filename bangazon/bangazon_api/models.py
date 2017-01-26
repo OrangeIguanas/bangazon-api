acec5b751f11ded0bd1a335972b2699dbd71fcd4
Peter Staggs <peter.staggs@csuglobal.edu>
2017-01-26 15:44:34 -0600

    added comments for clarification

--- bangazon/bangazon_api/models.py
+++ bangazon/bangazon_api/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+# This creates the model for Orders, which must be migrated subsequent to building the 
+# payment_types model to satisfy its foreign key requirement. 
 
 class Orders(models.Model): 
 	payment_types_id = models.ForiegnKey("payment_types_id")
