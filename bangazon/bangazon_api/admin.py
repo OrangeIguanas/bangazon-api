7e3afc19a22801c469d792d92cda210875e14b13
Ikechi <consumerike@gmail.com>
2017-01-29 18:16:02 -0600

    creater superuser,edited admin.py to register data models

--- bangazon/bangazon_api/admin.py
+++ bangazon/bangazon_api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from . models import Customers, Categories, Products
 # Register your models here.
+
+admin.site.register(Customers)
+admin.site.register(Categories)
+admin.site.register(Products)
