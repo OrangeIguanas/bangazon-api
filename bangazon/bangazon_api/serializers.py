261c637144024c73f8bac00291122deb70632375
Peter Staggs <peter.staggs@csuglobal.edu>
2017-01-30 10:26:29 -0600

    changed comment format

--- bangazon/bangazon_api/serializers.py
+++ bangazon/bangazon_api/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework import serializers
 from bangazon_api.models import *
 
-# Create a Serializer for the Order class to convert it to JSON format. 
-class OrdersSerializer(serializers.ModelSerializer):
-    class Meta:
+
+class OrdersSerializer(serializers.HyperlinkedModelSerializer):
+	''' purpose: convert model to JSON format
+		author: Pete
+		methods: Meta
+		Class: Orders
+		Fields: Include all 
+	'''
+	class Meta:
         model = Orders
         fields = ('payment_types_id',)
