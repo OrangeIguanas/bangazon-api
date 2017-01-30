fa3da492b8032238445e658c5ea5889fc5d39581
Zachary Cline <zcline08@gmail.com>
2017-01-30 10:14:40 -0600

    Added Documentation to Serializers

--- bangazon/bangazon_api/serializers.py
+++ bangazon/bangazon_api/serializers.py
@@ -3,11 +3,26 @@ from bangazon_api.models import Products
 from bangazon_api.models import Categories
 
 class ProductsSerializer(serializers.ModelSerializer):
+	""" 
+    ProductsSerializer class
+    The purpose of this class is to convert the Products data model to Json.
+    author: Zach
+    subclasses: Meta (contains fields that are to be converted)
+    
+    """
     class Meta:
         model = Products
         fields = ('name', 'price', 'description', 'quantity', 'category_Id', 'customer_Id')
 
 class CategoriesSerializer(serializers.ModelSerializer):
+	""" 
+    CategoriesSerializer class
+    The purpose of this class is to convert the Categories data model to Json.
+    author: Zach
+    subclasses: Meta (contains fields that are to be converted)
+    
+    """
+
 	class Meta:
 		model = Categories
 		fields = ('category_name',)
