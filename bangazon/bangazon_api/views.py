d47263effb3e5291b5390c9d88415334450ad549
Peter Staggs <peter.staggs@csuglobal.edu>
2017-02-01 12:42:51 -0600

    added viewSets, hyperlinked Serializers; actually works now.

--- bangazon/bangazon_api/views.py
+++ bangazon/bangazon_api/views.py
@@ -5,6 +5,8 @@ from rest_framework.renderers import JSONRenderer
 from rest_framework.parsers import JSONParser
 from .models import *
 from .serializers import *
+from rest_framework import viewsets
+
 
 
 class JSONResponse(HttpResponse):
@@ -23,367 +25,56 @@ class JSONResponse(HttpResponse):
         kwargs['content_type'] = 'application/json'
         super(JSONResponse, self).__init__(content, **kwargs)
 
-
-@csrf_exempt
-def product_list(request):
+class CustomerViewSet(viewsets.ModelViewSet):
     """
-    author: Ike
-    purpose: Show a list of products
-    arguments:
-    request- request object that extends HttpRequest, the request
-    represents an incoming HTTP request, including user-submitted data
-    and HTTP headers
-    
+    This viewset automatically provides `list` and `detail` actions.
     """
-    #try to retrieve product_list object if that
-    # list does not exist then display 404 error
-    try:
-        product_list = Products.objects.all()# Returns All product objects
-    except Products.DoesNotExist: #Handle Any Exceptions
-        return HttpResponse(status=404)
+    queryset = Customers.objects.all()
+    serializer_class = CustomersSerializer
 
-    if request.method == 'GET':
-        serializer = ProductsSerializer(product_list, many=True) #When getting many objects include many=True as a parameter
-        return JSONResponse(serializer.data)# Returning Data as Json
-
-    elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = ProductsSerializer(product_list, data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JSONResponse(serializer.data)
-        return JSONResponse(serializer.errors, status=400)
-
-    elif request.method == 'DELETE':
-        product_list.delete()
-        return HttpResponse(status=204)
-
-@csrf_exempt
-def single_product(request, product_id):
+class ProductViewSet(viewsets.ModelViewSet):
     """
-    author: Zach
-    purpose: Show a single product and product details
-    arguments:
-    request- request object that extends HttpRequest, the request
-    represents an incoming HTTP request, including user-submitted data
-    and HTTP headers
-    product_id - the unique ID for the given product
+    This viewset automatically provides `list` and `detail` actions.
     """
-    #try to retrieve product object that matches our product id, if that
-    # single_product does not exist then display 404 error
+    queryset = Products.objects.all()
+    serializer_class = ProductsSerializer
 
-    try: 
-        single_product = Products.objects.get(id = product_id)#Returns Products with A specific Id
-    except Products.DoesNotExist:
-        return HttpResponse(status=404)
 
-    if request.method == 'GET':
-        serializer = ProductsSerializer(single_product)
-        return JSONResponse(serializer.data)
-
-    elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = ProductsSerializer(single_product, data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JSONResponse(serializer.data)
-        return JSONResponse(serializer.errors, status=400)
-
-    elif request.method == 'DELETE':
-        single_product.delete()
-        return HttpResponse(status=204)
-
-@csrf_exempt
-def category_list(request):
+class CategoriesViewSet(viewsets.ModelViewSet):
     """
-    author: Zach
-    purpose: returns the Categories list for the bangazon_api app
-    or adds a Category to the Categories list
-    arguments:
-    request- request object that extends HttpRequest, the request
-    represents an incoming HTTP request, including user-submitted data
-    and HTTP headers
-
+    This viewset automatically provides `list` and `detail` actions.
     """
-    #try to retrieve category object that matches our category id, if that
-    # category does not exist then display 404 error
+    queryset = Categories.objects.all()
+    serializer_class = CategoriesSerializer
 
-
-    try:
-        category = Categories.objects.all()
-    except Products.DoesNotExist:
-        return HttpResponse(status=404)
-
-    if request.method == 'GET':
-        serializer = CategoriesSerializer(category, many=True)
-        return JSONResponse(serializer.data)
-
-    elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = ProductsSerializer(category, data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JSONResponse(serializer.data)
-        return JSONResponse(serializer.errors, status=400)
-
-    elif request.method == 'DELETE':
-        category.delete()
-        return HttpResponse(status=204)
-
-@csrf_exempt       
-def list_of_customers(request):
+class OrdersViewSet(viewsets.ModelViewSet):
     """
-    author: Ike
-    purpose: returns the Customers list for the bangazon_api app
-    or adds a customer to the customers list
-    arguments:
-        request- request object that extends HttpRequest, the request
-        represents an incoming HTTP request, including user-submitted data
-        and HTTP headers
+    This viewset automatically provides `list` and `detail` actions.
     """
+    queryset = Orders.objects.all()
+    serializer_class = OrdersSerializer
 
-    if request.method == 'GET':
-        customers = Customers.objects.all() #gets all customer objects
-        serializer = CustomersSerializer(customers, many=True)
-        return JSONResponse(serializer.data)
-
-    #Add customer to customer list (if request method is post)
-    elif request.method == 'POST':
-        #converts python data types into json
-        data = JSONParser().parse(request)
-        serializer = CustomersSerializer(data=data)
-        
-        #save customer to database if data structure is correct
-        if serializer.is_valid():
-            serializer.save()
-            return JSONResponse(serializer.data)
-        return JSONResponse(serializer.errors, status=400)
-      
-    #a context is a dictionary in which
-    #keys are names we'll use in the template to access the data and values are 
-    #the data we need to send to the template
-    context =  {'customers' : customers} 
-    
-
- # @csrf decorator (@) to denote that this function should be
- # run despite not having a CSRF token, which is is the cross-site request
- # forgery defense system
-
- 
-@csrf_exempt
-def customer(request, customer_id):
+class ProductOrdersViewSet(viewsets.ModelViewSet):
     """
-    author: Ike
-    purpose: Show a single customer and customer details
-    arguments:
-        request- request object that extends HttpRequest, the request
-        represents an incoming HTTP request, including user-submitted data
-        and HTTP headers
-        customer_id- the unique ID for the given customer
+    This viewset automatically provides `list` and `detail` actions.
     """
-    #try to retrieve customer object that matches our customer id, if that
-    # customer does not exist then display 404 error
-    try:
-        customer = Customers.objects.get(id=customer_id)
-    except Customers.DoesNotExist:
-        return HttpResponse(status=404)
-    
-    if request.method == 'GET':
-        # pass the customer object into the Serializer which will convert model
-        # data to python data type
-        serializer = CustomersSerializer(customer)
-        #convert data type to JSONResponse
-        return JSONResponse(serializer.data)
+    queryset = ProductHasOrders.objects.all()
+    serializer_class = ProductHasOrdersSerializer
 
-
-
-@csrf_exempt
-def order_list(request):
+class PaymentTypesViewSet(viewsets.ModelViewSet):
     """
-    author: Abby
-    purpose: Allow user to get("view") and post("create") data
-    arguments: request - Request extends Django's HttpRequest
-    
+    This viewset automatically provides `list` and `detail` actions.
     """
-    if request.method == 'GET':
-        order = Orders.objects.all()
-        serializer = OrdersSerializer(order, many=True)
-        return JSONResponse(serializer.data)
-
-    elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = OrdersSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JSONResponse(serializer.data, status=201) #201 - Request Fulfilled
-        return JSONResponse(serializer.errors, status=400) #400 - Bad Request
-
-
-
-
-@csrf_exempt
-def order_detail(request, pk):
-    """
-    author: Abby
-    purpose: Allow user to get("view") and put("update") and delete data
-    arguments: request - Request extends Django's HttpRequest, pk - Primary Key
-    
-    """
-
-    try:
-        order = Orders.objects.get(pk=pk)
-    except Orders.DoesNotExist:
-        return HttpResponse(status=404) #404 - Not found
-
-    if request.method == 'GET':
-        serializer = OrdersSerializer(order)
-        return JSONResponse(serializer.data)
-
-    elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = OrdersSerializer(order, data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JSONResponse(serializer.data)
-        return JSONResponse(serializer.errors, status=400) #400 - Bad Request
-
-    elif request.method == 'DELETE':
-        order.delete()
-        return HttpResponse(status=204) #204 - Fulfilled - No Additional Content
-
-@csrf_exempt
-def payment_types_list(request):
-    """
-    author: Pete
-    purpose: Show a list of products
-    arguments:
-    request- request object that extends HttpRequest, the request
-    represents an incoming HTTP request, including user-submitted data
-    and HTTP headers
-    
-    """
-    #try to retrieve product_list object if that
-    # list does not exist then display 404 error
-    try:
-        payment_types_list = PaymentType.objects.all()# Returns All product objects
-    except PaymentType.DoesNotExist: #Handle Any Exceptions
-        return HttpResponse(status=404)
-
-    if request.method == 'GET':
-        serializer = PaymentTypesSerializer(payment_types_list, many=True) #When getting many objects include many=True as a parameter
-        return JSONResponse(serializer.data)# Returning Data as Json
-
-    elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = PaymentTypesSerializer(payment_types_list, data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JSONResponse(serializer.data)
-        return JSONResponse(serializer.errors, status=400)
-
-    elif request.method == 'DELETE':
-        payment_types_list.delete()
-        return HttpResponse(status=204)
-
-
-def single_payment_type(request, pk):
-    """
-    author: Pete
-    purpose: Show a single product and product details
-    arguments:
-    request- request object that extends HttpRequest, the request
-    represents an incoming HTTP request, including user-submitted data
-    and HTTP headers
-    product_id - the unique ID for the given product
-    """
-    #try to retrieve product object that matches our product id, if that
-    # single_product does not exist then display 404 error
-
-    try: 
-        single_payment_type = PaymentType.objects.get(pk=pk)#Returns Products with A specific Id
-    except PaymentType.DoesNotExist:
-        return HttpResponse(status=404)
-
-    if request.method == 'GET':
-        serializer = PaymentTypesSerializer(single_payment_type)
-        return JSONResponse(serializer.data)
-
-    elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = PaymentTypesSerializer(single_payment_type, data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JSONResponse(serializer.data)
-        return JSONResponse(serializer.errors, status=400)
-
-    elif request.method == 'DELETE':
-        single_product.delete()
-        return HttpResponse(status=204)
+    queryset = PaymentType.objects.all()
+    serializer_class = PaymentTypesSerializer
 
 
 
 
 
-@csrf_exempt
-def productorders_list(request):
-
-
-    """
-    purpose: Allow user to get("view") and post("create") data
-    arguments: request - Request extends Django's HttpRequest
-    author: Shawn
-    """
-    try:
-        productorders_list = ProductHasOrders.objects.all()# Returns All product objects
-    except ProductHasOrders.DoesNotExist: #Handle Any Exceptions
-        return HttpResponse(status=404)
-
-
-    if request.method == 'GET':
-        product = ProductHasOrders.objects.all()
-        serializer = ProductHasOrdersSerializer(product, many=True)
-        return JSONResponse(serializer.data)
-
-    elif request.method == 'POST':
-            data = JSONParser().parse(request)
-            serializer = ProductHasOrdersSerializer(data=data)
-            if serializer.is_valid():
-                serializer.save()
-                return JSONResponse(serializer.data, status=201)
-            return JSONResponse(serializer.errors, status=400)
 
 
 
 
-@csrf_exempt
-def productorders_detail(request, pk):
 
-    """
-    purpose: Allow user to get("view") and put("update") and delete data
-    arguments: request - Request extends Django's HttpRequest,pk - Primary Key
-    author: Shawn
-    """
-
-
-    try:
-        single_productorders = ProductHasOrders.objects.get(pk=pk)
-    except ProductHasOrders.DoesNotExist:
-        return HttpResponse(status=404) #not found error
-
-    if request.method =='GET':
-        serializer = ProductHasOrdersSerializer(single_productorders)
-        return JSONResponse(serializer.data)
-
-    elif request.method =='PUT':
-        data = JSONParser().parse(request)
-        serializer = ProductHasOrdersSerializer(single_productorders, data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JSONResponse(serializer.data)
-        return JSONResponse(serializer.errors, status=400)
-
-    elif request.method == 'DELETE':
-        single_productorders.delete()
-        orders.delete()
-        return HttpResponse(status=204) #fulfilled request 
 
